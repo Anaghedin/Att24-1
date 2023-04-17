47ac4f9a0ef8e05979b4f3efc35b7a7933323411
Ana Flávia Ghedin Rosso <91229363+Anaghedin@users.noreply.github.com>
2023-04-17 08:59:09 -0300

    Update ATV24.java

--- ATV24.java
+++ ATV24.java
@@ -1,15 +1,25 @@
 
 import java.util.Scanner;
 
-public class ATV24 {
+public class att24 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.print("Quantos números você deseja verificar? ");
         int n = sc.nextInt();
 
         for (int i = 1; i <= n; i++) {
-            System.out.print("Digite o " + i + "º número: ");
-            int num = sc.nextInt();
+            int num = 0;
+            boolean validInput = false;
+            while (!validInput) {
+                System.out.print("Digite o " + i + "º número: ");
+                String input = sc.next();
+                if (input.matches("-?\\d+")) {
+                    num = Integer.parseInt(input);
+                    validInput = true;
+                } else {
+                    System.out.println("Entrada inválida - Digite apenas números");
+                }
+            }
 
             if (num > 0) {
                 System.out.println(num + " é positivo.");
